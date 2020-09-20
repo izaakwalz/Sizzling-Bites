897799f29d3fba87c2a4826579da5972cb4e5f37
izaakwalz <iblack.xyz@gmail.com>
2020-09-20 01:39:15 +0100

    Initial commit app setup and splash screen

---
+++ app/src/main/java/com/example/sizzlingbites/Splash.java
@@ -0,0 +1,33 @@
+package com.example.sizzlingbites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Splash extends AppCompatActivity {
+
+    ImageView logo, appName, splashImg;
+    LottieAnimationView lottieAnimationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.logo);
+        appName = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.img);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
+
+    }
+}
